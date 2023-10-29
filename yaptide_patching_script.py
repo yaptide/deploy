5a22cb2401e470c596ad975ee2fc25f941ee2fb6
Łukasz Pitrus <pitrus@student.agh.edu.pl>
2023-10-29 15:55:07 +0100

    Added yaptide patching script

---
+++ yaptide_patching_script.py
@@ -0,0 +1,41 @@
+# use bash script to get all PR patches from github
+# install gh cli tool before running this script
+# login to gh cli tool before running this script
+import subprocess
+import json
+
+from pathlib import Path
+from tqdm import tqdm
+from zipfile import ZipFile
+
+
+def main():
+    base_dir = Path(__file__).resolve().parent
+    patches_dir = base_dir / "patches"
+    patches_dir.mkdir(exist_ok=True)
+
+    for repo_name in ["ui", "yaptide", "converter"]:
+        repo = f"yaptide/{repo_name}"
+        # get all PRs with EuroHPC label and closed state
+        output = subprocess.check_output(
+            f"gh pr --repo {repo} list --label EuroHPC --state all --json number --limit 10000", shell=True, text=True)
+        # read json file
+        data = json.loads(output)
+        print(f"Number of patches for {repo} repo: {len(data)}")
+
+        # get patches for each PR
+        for i, PR in enumerate(tqdm(data, desc=f"Getting patches for {repo_name} repo", unit="patch")):
+            patch = subprocess.check_output(
+                f'gh pr --repo {repo} diff {PR["number"]} --patch', shell=True, text=True)
+
+            with open(patches_dir / f'patch_{repo_name}_{PR["number"]}.patch', 'w') as f:
+                f.write(patch)
+
+    zip_file = base_dir / "patches.zip"
+    with ZipFile(zip_file, 'w') as zipObj:
+        for file in patches_dir.iterdir():
+            zipObj.write(file)
+
+
+if __name__ == "__main__":
+    main()
